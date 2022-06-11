4ddea7cffc8acd013238c635f39c32ebaa8907f7
Narvils <gustavs.gn@gmail.com>
2022-06-11 20:01:55 +0300

    Maciba matrials otrais piemers

---
+++ src/Macibu_matrials_piemers_2.java
@@ -0,0 +1,14 @@
+
+public class Macibu_matrials_piemers_2 {
+	public static void main(String[] args) {
+		char viens = 'L';
+		char divi = 'a';
+		char tris = 'm';
+		char cetri = 'p';
+		char pieci = 'a';
+		char sesi = '!';
+		String vards = "Lampa!"; 
+		System.out.println("1.Char: "+viens+""+divi+""+tris+""+cetri+""+pieci+""+sesi);
+		System.out.println("2.String: "+vards);
+	}
+}
