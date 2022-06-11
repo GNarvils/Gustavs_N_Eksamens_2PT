9ad1527be824ce5faf66b051d6e1a8f9a9aba9c7
Narvils <gustavs.gn@gmail.com>
2022-06-11 20:20:58 +0300

    Macibu matrials tresais piemers

---
+++ src/Macibu_matrials_piemers_3.java
@@ -0,0 +1,8 @@
+
+public class Macibu_matrials_piemers_3 {
+	public static void main(String[] args) {
+		short lielaVertiba = 130;
+		byte mazaVertiba = (byte) lielaVertiba;	
+		System.out.print(mazaVertiba);
+	}
+}
---
+++ src/Macibu_matrials_piemers_4.java
@@ -0,0 +1,9 @@
+
+public class Macibu_matrials_piemers_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
