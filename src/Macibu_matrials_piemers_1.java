fdb2538531b5664ce7d9e499c672c6e41732ae0a
Narvils <gustavs.gn@gmail.com>
2022-06-11 18:20:18 +0300

    Maciba matrials pirmais piemers

---
+++ src/Macibu_matrials_piemers_1.java
@@ -0,0 +1,9 @@
+
+public class Macibu_matrials_piemers_1 {
+	public static void main(String[] args) {
+		int Cipars1 = 5;
+		int Cipars2 = 2;
+		double summa = Cipars1+Cipars2;
+		System.out.print(summa);
+	}
+}
