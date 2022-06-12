385adb676b26123755342dcce3f6cb63e0868115
Narvils <gustavs.gn@gmail.com>
2022-06-12 18:24:27 +0300

    Testa faila izveidošana un pazinojuma parādīšana

---
+++ src/Tests.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class Tests {
+    public static void pazinojums(){
+    	JOptionPane.showMessageDialog(null, "Esiet sveicināti elektroniskā testā, kurš pārbaudīs jūsu zināšanas par programmēšanas valodas Java mainīgajiem un to datu tipiem!\nTestā būs jautājumi kurām ir vairākas atbildes!\nJa iesniegtu atbildi uz jautājuma ierakstiet teksta logā jūsu izvēlētās atbildes ciparu tad atstarpi un velvienu ciparu!\nSpiediet pogu ar apzīmējumu OK lai tūrpinātu!");
+    }
+	public static void main(String[] args) {
+		pazinojums();
+
+	}
+
+}
