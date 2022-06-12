41948cc53e3533934aa81ec009f44b1a6ee11c1c
Narvils <gustavs.gn@gmail.com>
2022-06-12 18:47:04 +0300

    Pirmais jautajums pareizi uzstaisits

--- src/Tests.java
+++ src/Tests.java
@@ -1,38 +1,40 @@
 import javax.swing.JOptionPane;
 
 public class Tests {
-    public static void pazinojums(){
+	static int pareizi = 0;
+	static boolean pareizi1 = false;
+	static boolean pareizi2 = false;
+	static boolean pareizi3 = false;
+	static boolean pareizi4 = false;
+	static boolean pareizi5 = false;
+	static boolean pareizi6 = false;
+	static boolean pareizi7 = false;
+	static boolean pareizi8 = false;
+	static boolean pareizi9 = false;
+	static boolean pareizi10 = false;
+	public static void pazinojums(){
     	JOptionPane.showMessageDialog(null, "Esiet sveicināti elektroniskā testā, kurš pārbaudīs jūsu zināšanas par programmēšanas valodas Java mainīgajiem un to datu tipiem!"
     			+ "\nTestā būs jautājumi kurām ir vairākas atbildes!"
-    			+ "\nJa iesniegtu atbildi uz jautājuma ierakstiet teksta logā jūsu izvēlētās atbildes ciparu tad atstarpi un velvienu ciparu!"
+    			+ "\nJa iesniegtu atbildi uz jautājuma, rakstiet visus atbilžu variantus kopā!"
+    			+ "\nPiemēram atbilde ir pirmais un otrais variants tad jūs rakstīsiet 12 vai 21"
     			+ "\nSpiediet pogu ar apzīmējumu OK lai sāktu testu!");
     }
-    public static int Jautajums1(int x, boolean pareizi){
-        String atbilde = JOptionPane.showInputDialog("1. Jautājums\n Ar kuriem mainīgajiem var veikt matemātiskās darbības!"
+    public static void Jautajums1(){
+        int atbilde = Integer.parseInt(JOptionPane.showInputDialog("1. Jautājums\n Ar kuriem mainīgajiem var veikt matemātiskās darbības!"
         		+ "\n 1. Int "
         		+ "\n 2. Char "
         		+ "\n 3. Double "
-        		+ "\n 4. Boolean");
-        if(atbilde == "1 3" || atbilde == "3 1") {
-        	x = x + 1;
-        	pareizi = true;
+        		+ "\n 4. Boolean"));
+        if(atbilde == 13 || atbilde == 31) {
+        	pareizi = pareizi + 1;
+        	pareizi1 = true;
         }
-		return x;
     }
 	public static void main(String[] args) {
-		int punkti = 0;
-		boolean pareizi1 = false;
-		boolean pareizi2 = false;
-		boolean pareizi3 = false;
-		boolean pareizi4 = false;
-		boolean pareizi5 = false;
-		boolean pareizi6 = false;
-		boolean pareizi7 = false;
-		boolean pareizi8 = false;
-		boolean pareizi9 = false;
-		boolean pareizi10 = false;
 		pazinojums();
-        Jautajums1(punkti, pareizi1);
+        Jautajums1();
+        System.out.println(pareizi1+""+pareizi);
+       
 	}
 
 }
