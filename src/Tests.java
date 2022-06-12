70ad6f50b8848a24f87e5b365a1ebd73b6b0c471
Narvils <gustavs.gn@gmail.com>
2022-06-12 18:52:56 +0300

    Otrais Jautajums

--- src/Tests.java
+++ src/Tests.java
@@ -28,12 +28,25 @@ public class Tests {
         if(atbilde == 13 || atbilde == 31) {
         	pareizi = pareizi + 1;
         	pareizi1 = true;
+        	System.out.println("Pareizi");
+        }
+    }
+    public static void Jautajums2(){
+        int atbilde = Integer.parseInt(JOptionPane.showInputDialog("2. Jautājums\n Kuras no šīm mainīgo datu tipiem varētu izmantot lai saglabātu !"
+        		+ "\n 1. Byte "
+        		+ "\n 2. Short "
+        		+ "\n 3. Int "
+        		+ "\n 4. Long"));
+        if(atbilde == 234 || atbilde == 243 || atbilde == 324 || atbilde == 342 || atbilde == 423 || atbilde == 432) {
+        	pareizi = pareizi + 1;
+        	pareizi2 = true;
+        	System.out.println("Pareizi");
         }
     }
 	public static void main(String[] args) {
 		pazinojums();
         Jautajums1();
-        System.out.println(pareizi1+""+pareizi);
+        Jautajums2();
        
 	}
 
