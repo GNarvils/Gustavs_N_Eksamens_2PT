7d31fb4a44859d593c0dc3f24f6a4104facb1898
Narvils <gustavs.gn@gmail.com>
2022-06-13 20:07:47 +0300

    Izdesu mācība matriala piemerus

--- src/Macibu_matrials_piemers_1.java
+++
@@ -1,9 +0,0 @@
-
-public class Macibu_matrials_piemers_1 {
-	public static void main(String[] args) {
-		int Cipars1 = 5;
-		int Cipars2 = 2;
-		double summa = Cipars1+Cipars2;
-		System.out.print(summa);
-	}
-}
